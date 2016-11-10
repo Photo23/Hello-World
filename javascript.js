41c6aabf20f98349ea3e07a50fa2e2008293faa7
Photo23 <jonbush23@gmail.com>
2016-11-10 16:30:39 -0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -17,16 +17,39 @@ $(document).ready(function(){
         drawer.classList.remove('open');
       });
 
-      var counter = 0;
-      
+      //var counter = 0;
+
+      /* 
+       * unhides startbox after 3 seconds
+       */
       var startbox = document.querySelector('#startbox');
+      var photobox = document.querySelector('.photobox');
+      var photocontainer = document.querySelector('#photocontainer');
 
       function removeHide() {
-      	
         startbox.classList.remove('hide');
       };
 
-     
       setTimeout(removeHide, 3000);
+
+
+      startbox.addEventListener('click', function(){
+      		startbox.classList.add('hide');
+
+      		setTimeout(function(){
+      			startbox.classList.add('displaynone');
+      			photocontainer.classList.remove('displaynone');
+      			
+
+      		}, 1000);
+
+      		setTimeout(function(){
+      			
+      			photocontainer.classList.add('open');
+      		}, 2000);
+      		
+
+
+      });
 
 });
